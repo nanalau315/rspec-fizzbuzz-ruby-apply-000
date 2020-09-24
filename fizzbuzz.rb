acfb0be6dd723ca9ae87c4cfeb1f2712bdf0d584
Chamaeleonidae Na <nanalau315@hotmail.com>
2020-09-24 19:16:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number)
+  if number / 3 == 1
+    puts "Fizz"
+    "Fizz"
+  elsif number / 5 == 1
+    puts "Buzz"
+    "Buzz"
+  elsif number / 3 || number / 5 == 5 || 3
+    puts "FizzBuzz"
+    "FizzBuzz"
+  else number / 3 != 1
+    puts "nil"
+    
+end
