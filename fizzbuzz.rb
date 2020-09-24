69c673f572858f93c49523e0316c687928bbabd2
Chamaeleonidae Na <nanalau315@hotmail.com>
2020-09-24 19:28:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -11,7 +11,7 @@ def fizzbuzz(number)
   elsif number / 3.0 || number / 5.0 == 5.0 || 3.0
     puts "FizzBuzz"
     "FizzBuzz"
-  else number / 3.0 || number / 5.0 != 1.00
+  else number / 3.0 || number / 5.0 != 5.0 || 3.0
     puts "nil"
     "nil"
   end
